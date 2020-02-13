45353b3b2de357a22e078e655d8d66a88b58f049
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-02-13 16:36:12 -0600

    Create Program1.cs

---
+++ Program1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting System");
+        Console.WriteLine("Remember, we're acounting on you!");
+        Console.ReadLine();
+        }
+    }
+
