b3086ae8f87af9e3d25a70d459bb4a63af6a5aa0
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-05 17:41:03 -0600

    whileAssignment

---
+++ whileAssignment/whileAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int z = 80;
+            //while (z < 100)
+            //{
+               // Console.WriteLine(z);
+               // z++;
+               // Console.ReadLine();
+
+
+                int B = 31;
+            do
+            {
+                Console.WriteLine(B);
+                B++;
+            }
+            while (B < 55);
+            Console.ReadLine();
+
+
+
+
+
+        //}
+        }
+    }
+}
+
+
+
+
+   
