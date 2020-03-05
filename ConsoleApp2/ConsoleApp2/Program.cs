13de8543978a718f8fb9c21c75d90e63a293e12c
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-05 09:05:16 -0600

    ACMEprj

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember,we're 'accounting'on you!");
+            Console.ReadLine();
+        }
+    }
+}
