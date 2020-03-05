1798774fc0270969df593e023abd206818bacff9
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-05 10:28:22 -0600

    approval

---
+++ ApprovalProgram/ApprovalProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprovalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 15;
+             int tickets = 3;
+           
+
+
+            Console.WriteLine("What is your age?");
+            int UserAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a dui?");
+            bool Answer = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many tickets do you have?");
+            int UserTickets = Convert.ToInt32(Console.ReadLine());
+            bool Applicant = (UserAge > age && Answer && UserTickets < tickets);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(Applicant);
+            Console.ReadLine();
+        }
+    }
+}
