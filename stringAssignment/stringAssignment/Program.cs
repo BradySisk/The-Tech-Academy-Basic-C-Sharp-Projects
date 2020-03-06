7c879c9c321a0f34d4f1b2d581b22089ad07b625
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-05 20:10:19 -0600

    stringAssignment

---
+++ stringAssignment/stringAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string Concat = "Hello my name is" + "Brady, i am a software" + "developer.";
+            // Console.WriteLine(Concat);
+            // Console.ReadLine();
+
+            // string name = "brady";
+            // name = name.ToUpper();
+            // Console.WriteLine(name);
+            // Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append ("My name is Brady.\n I'm hoping to become a successful software developer. \n I would also like to work on video games at some point, maybe even make my own.");
+
+           
+           
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
