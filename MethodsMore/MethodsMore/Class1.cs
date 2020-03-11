85e9cfcf82c0f24fbb775efa5ab2f2a5f14dbbc3
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-11 01:36:31 -0500

    Method

---
+++ MethodsMore/MethodsMore/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsMore
+{
+    class Class1
+
+    {
+        public int Multiply(int number)
+        {
+            int result1 = number * 2;
+            return result1;
+        }
+
+        public int Add(int number)
+        {
+            int result2 = number + 2;
+            return result2;
+        }
+
+        public int Subtract(int number)
+        {
+            int result3 = number - 2;
+            return result3;
+        }
+
+
+    }
+}
---
+++ MethodsMore/MethodsMore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsMore
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a number to math operations with.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Class1 Math = new Class1();
+            Math.Multiply(number);
+            Math.Subtract(number);
+            Math.Add(number);
+
+            int result1 = Math.Multiply(number);
+            int result2 = Math.Add(number);
+            int result3 = Math.Subtract(number);
+
+            Console.WriteLine($"{ result1} { result2} { result3}");
+            Console.ReadLine();
+
+
+
+
+           
+        }
+    }
+}
