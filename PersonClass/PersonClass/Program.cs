8a4175216ddb54dfec020bac2f3d8ed0ad3725db
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-12 13:11:11 -0500

    PersonClass

---
+++ PersonClass/PersonClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonClass
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+          Person person = new Person();
+            person.FirstName = "Lost";
+            person.LastName = "Sanity";
+            person.SayName();
+            Console.ReadLine();
+
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.ID = 54;
+            employee.SayName();
+            Console.ReadLine();
+
+
+           
+
+           
+           
+
+           
+
+           
+         
+        }
+    }
+}
