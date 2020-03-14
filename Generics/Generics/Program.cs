21b54a8c19357996f3be1a9d29225e48a9e2c181
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-14 18:16:57 -0500

    Generics1

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -8,15 +8,19 @@ namespace Generics
 {
     class Program
     {
-      public  static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Employee<int> employee = new Employee<int>();
             employee.things = new List<int>();
             employee.things.Add(88);
             employee.things.Add(46);
             employee.things.Add(21);
+            foreach (int num in employee.things)
+            {
+                Console.WriteLine(num);
+            }
 
-            Employee2<string> employee2 = new Employee2<string>();
+            Employee<string> employee2 = new Employee<string>();
             employee2.things = new List<string>();
             employee2.things.Add("Bobby");
             employee2.things.Add("Rupert");
@@ -24,17 +28,10 @@ namespace Generics
             foreach (string add in employee2.things)
             {
                 Console.WriteLine(add);
-                Console.ReadLine();
-            }
 
-            for (int i = 0; i < employee2.things.Length; i++) 
-            {
-                string add = employee2.things[i];
-                Console.WriteLine(add);
-                Console.ReadLine();
             }
-            
-          
-        }
+            Console.ReadLine();
+        } 
+       
     }
 }
