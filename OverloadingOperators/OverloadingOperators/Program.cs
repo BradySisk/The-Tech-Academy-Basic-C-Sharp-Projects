ae0ebdcc61d41178ae23667736b4fbcd65d4aaa2
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-13 20:37:53 -0500

    OverloadingOperators

---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Bob";
+            employee.lastName = "Bobby";
+            employee.ID = 55;
+
+            Employee2 employee2 = new Employee2();
+            employee2.firstName = "Dale";
+            employee2.lastName = "Daley";
+            employee2.ID = 88;
+
+            Console.WriteLine(employee.ID == employee2.ID);
+            Console.ReadLine();
+        }
+     
+
+            
+
+    }
+}
