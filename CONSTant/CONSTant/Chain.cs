74fcd454cb7759da43ac32f5ded29e51b440d5d4
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-15 08:05:13 -0500

    Constant

---
+++ CONSTant/CONSTant/Chain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTant
+{
+    
+   public class Customer
+    {
+        public int Id;
+        public string Name;
+        public const string idk = "I dont know";
+
+
+        public Customer( int id) : this(id,"1")
+        {
+            
+        }
+        
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        }
+     
+       
+            
+           
+           
+       
+    }
+
---
+++ CONSTant/CONSTant/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTant
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(5);
+            
+        }
+    }
+}
