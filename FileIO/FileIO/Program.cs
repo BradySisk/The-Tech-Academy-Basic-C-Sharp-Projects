aba098f1ed93b1d66ed3ea0760d6e7f1f340405e
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-15 02:27:48 -0500

    FILEIO

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Mark Wahlbang\Logs\Log.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+            string text = File.ReadAllText("C:\\Users\\Mark Wahlbang\\Logs\\Log.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
+
+
