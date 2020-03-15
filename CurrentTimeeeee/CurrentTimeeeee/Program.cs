6d123057c28e08203a1f695e2ce337164b361f25
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-03-15 04:13:49 -0500

    DateTime

---
+++ CurrentTimeeeee/CurrentTimeeeee/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrentTimeeeee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+
+
+
+
+            Console.WriteLine(now);
+            Console.WriteLine("Please enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine(now.AddHours(userInput).ToString());
+            Console.ReadLine();
+        }
+    }
+}
