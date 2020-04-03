49315025214989b17a08db6ed43838f36db7cb12
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-04-03 14:39:57 -0500

    days of week

---
+++ DaysOfTheWeek/DaysOfTheWeek/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+       
+        enum DaysOfTheWeek { Monday , Tuesday, Wednsday, Thursday, Friday, Saturday, Sunday  };
+
+        public static void Main()
+        {
+            Console.WriteLine("please enter the current day of the week.");
+            
+            
+            string DaysOfTheWeek = Console.ReadLine();
+          
+            {
+               try
+                {
+                    
+                  DaysOfTheWeek Friday = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek),DaysOfTheWeek);
+                    Console.WriteLine(Friday);
+                    Console.WriteLine("You entered:" + Friday);
+                    Console.ReadLine();
+
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                    Console.ReadLine();
+                }
+                
+            }
+          
+        }
+    }
+}
+
