14879e6d38b71d0bf44f646049b430c35623cb01
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-04-07 19:01:51 -0500

    loopsUserInput

---
+++ loopUserinput/loopUserinput/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopUserinput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(5);
+            numbers.Add(7);
+
+
+
+
+            try
+            {
+                foreach (int element in numbers)
+                {
+                    Console.WriteLine("Math operation result: " + (element / userInput));
+                    Console.ReadLine();
+                }
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+               
+
+            }
+            catch (FormatException z)
+            {
+                Console.WriteLine("Error: " + z.Message);
+
+            }
+
+            Console.WriteLine("Thank for your time and hope you enjoyed the program, have a good day.");
+            Console.ReadLine();
+        }
+    }
+}
